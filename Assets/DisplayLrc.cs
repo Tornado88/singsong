35d292cffbf6eeb5f484d4978632fda1b460b82b
Tornado88 <liangxiaolong06@126.com>
2017-03-07 12:21:55 +0800

    好使了 还是有一些小bug 和等待改进的地方

--- Assets/DisplayLrc.cs
+++ Assets/DisplayLrc.cs
@@ -19,16 +19,11 @@ public class DisplayLrc : MonoBehaviour
     private Encoding enc; //更改歌词字符编码
     private StreamReader sr; //读取歌词
     public System.Collections.Generic.List<string> strList = new System.Collections.Generic.List<string>();//获取到的歌词
-    public System.Collections.Generic.List<string> timList = new System.Collections.Generic.List<string>();//获取到的时间
-    public System.Collections.Generic.List<string> strTimList = new System.Collections.Generic.List<string>();//歌词单个字的持续时间
-    //private ArrayList[] list;
-    public int strindex; //歌词行数
-    public int timindex; //歌曲行数
-    public int strtimes;//所有歌词单字的持续时间
-    public int x = 0; //获取的歌词当前的行数
+    public System.Collections.Generic.List<float> timList = new System.Collections.Generic.List<float>();//获取到的时间
+
+    
     public string path; //歌词路径
-    public int m_Minutes; //分钟
-    public int m_Seconds; //秒
+
 
 
     private void Start()
@@ -39,7 +34,7 @@ public class DisplayLrc : MonoBehaviour
         sr = new StreamReader(path, enc);
         OpenFile(); //打开歌词文件
 
-
+        mp3.Play();
     }
 
     //打开歌词文件
@@ -53,7 +48,6 @@ public class DisplayLrc : MonoBehaviour
                 oneLine = sr.ReadLine() + "\r\n";
                 Replacestr(oneLine);
                 Time(oneLine);
-                StrTime(oneLine);
             }
             sr.Close();
         }
@@ -77,9 +71,7 @@ public class DisplayLrc : MonoBehaviour
             if (lrcTxt != null)//读取当前行不为空
             {
                 lrcTxt = Regex.Replace(lrcTxt, pattner, "");//把歌词中的<>去除掉
-                //strList[strindex] = lrcTxt;
                 strList.Add(lrcTxt);
-                strindex++;//下一行
             }
         }
     }
@@ -100,61 +92,34 @@ public class DisplayLrc : MonoBehaviour
         Regex reg = new Regex(sPattner);
         foreach (Match mc in reg.Matches(t))
         {
-            c = mc.Groups["t"].ToString() + "\r\n";
-            if (c != null)
-            {
-                //timList[timindex] = c.Substring(1, 5);
-                timList.Add(c.Substring(1, 5));
-                timindex++;
-            }
+            string[] tmpStrs = mc.Value.Split(new char[] { '[', ':', ']' });
+            float tmpf = Convert.ToSingle(tmpStrs[1]) * 60 + Convert.ToSingle(tmpStrs[2]);
+            timList.Add(tmpf);
         }
     }
 
 
-    //获取字符持续时间
-    public void StrTime(string t)
-    {
-        string b = null;
-        string sPattner = @"(?<q><[^>]*)";
-        Regex reg = new Regex(sPattner);
-        foreach (Match mc in reg.Matches(t))
-        {
-            b = mc.Groups["q"].ToString() + "\r\n";
-            if (b != null)
-            {
-                //strTimList[strtimes] = b.Substring(1, 5);
-                strTimList.Add(b.Substring(1, 5));
-                float times = UnityEngine.Time.deltaTime;
-                strtimes++;
-            }
-        }
-    }
-
-
-
-
+    private int x = 0; //获取的歌词当前的行数
     //匹配时间
     private void SuitedTime()
     {
-        string p = (m_Minutes.ToString("00") + ":" + m_Seconds.ToString("00"));
-        //print(p);
-        for (int i = x; i < timindex; i++)
+        float tmpf = mp3.time;
+        for (int i = x; i < timList.Count-1; i++)
         {
-            if (timList[i] == p)
+            if (timList[i] <= tmpf)
             {
                 MusicLrc.text = strList[i];
-                MusicLrc1.text = strList[i];
+                MusicLrc1.text = strList[i+1];
                 x = i + 1;
-                //rollTest._instance.UpdatePlane();
             }
+            else
+                break;
         }
     }
 
     private void Update()
     {
-        m_Minutes = (int)mp3.time / 60;//计算音乐当前播放分钟
-        m_Seconds = (int)mp3.time % 60;//计算音乐当前播放秒
-        SuitedTime();//匹配时间
+        SuitedTime();//匹配当前时间和歌词
     }
 
     private static Encoding GetEncoding(string file, Encoding defEnc)
